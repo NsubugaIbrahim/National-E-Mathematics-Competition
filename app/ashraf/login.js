1527ce2c29fd8e10b77e7bf34e07e7ec6d3ce9bd
AshrafGit256 <ashrafwalusimbi27@gmail.com>
2024-07-25 14:21:50 +0300

    Login.js updated

--- app/ashraf/login.js
+++ app/ashraf/login.js
@@ -11,3 +11,16 @@ loginBtn.addEventListener('click', () => {
     login_container.classList.remove("active");
 });
 
+
+const login_container = document.getElementById('login_container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    login_container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    login_container.classList.remove("active");
+});
+
