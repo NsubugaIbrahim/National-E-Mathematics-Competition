9b321df156732f393f8ce614751187e4fc661209
NsubugaIbrahim <ibrahimnazel942@gmail.com>
2024-07-25 12:10:03 +0300

    sidebar

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -66,12 +66,6 @@
                     <h4>{{ __('Analytics') }}</h4>
                 </a>
             </li>
-            <li class=" {{ $pageSlug == 'upgrade' ? 'active' : '' }} bg-info">
-                <a href="{{ route('pages.upgrade') }}">
-                    <i class="tim-icons icon-spaceship"></i>
-                    <p>{{ __('Upgrade to PRO') }}</p>
-                </a>
-            </li>
         </ul>
     </div>
 </div>
