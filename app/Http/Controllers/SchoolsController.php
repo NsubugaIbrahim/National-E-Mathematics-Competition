7c3805227dcd0345f560f80bd4d504d099ce7dc2
kayiwarahim <kayiwarahim@gmail.com>
2024-07-26 12:00:08 +0300

    modified the controllers

--- app/Http/Controllers/SchoolsController.php
+++ app/Http/Controllers/SchoolsController.php
@@ -22,7 +22,7 @@ class SchoolsController extends Controller
       
           // Loop through each row's data and insert into database
           for ($i = 0; $i < count($schoolRegNo); $i++) {
-            $pdo = new PDO('mysql:host=localhost;dbname=laravel', 'root', '');
+            $pdo = new PDO('mysql:host=localhost;dbname=mathchallenge', 'root', '');
             $sql = "INSERT INTO schools(schoolRegNo, schoolName, district) VALUES (?, ?, ?)";
             $stmt = $pdo->prepare($sql);
             $stmt->execute(array($schoolRegNo[$i], $schoolName[$i], $district[$i]));
