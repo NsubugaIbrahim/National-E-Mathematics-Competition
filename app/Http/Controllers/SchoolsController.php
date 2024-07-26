d2611054d7d5dac499a28a299b9975205d504620
NsubugaIbrahim <ibrahimnazel942@gmail.com>
2024-07-26 11:40:14 +0300

    Schools Controller from Sherry Namembwa

--- app/Http/Controllers/SchoolsController.php
+++ app/Http/Controllers/SchoolsController.php
@@ -4,33 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use PDO; //import PDO
+use App\Models\School;
 
 class SchoolsController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');      
+        $this->middleware('auth');
     }
-    
-    public function store(Request $request) {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-          // Access form data as arrays
-            $schoolRegNo = $request->input('regno');
-            $schoolName = $request->input('name');
-            $district = $request->input('district');
-      
-          // Loop through each row's data and insert into database
-          for ($i = 0; $i < count($schoolRegNo); $i++) {
-            $pdo = new PDO('mysql:host=localhost;dbname=laravel', 'root', '');
-            $sql = "INSERT INTO schools(schoolRegNo, schoolName, district) VALUES (?, ?, ?)";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute(array($schoolRegNo[$i], $schoolName[$i], $district[$i]));
-          }
-      
-          // Redirect to the success page
-          return redirect()->back()->with('status', 'School(s) successfully registered');
+
+    public function store(Request $request)
+    {
+        // Access form data
+        $schoolRegNo = $request->input('regno');
+        $schoolName = $request->input('name');
+        $district = $request->input('district');
+        
+        // Loop through each row's data and create a new School instance
+        for ($i = 0; $i < count($schoolRegNo); $i++) {
+            School::create([
+                'schoolRegNo' => $schoolRegNo[$i],
+                'schoolName' => $schoolName[$i],
+                'district' => $district[$i],
+            ]);
         }
-      }
+        
+        // Redirect to the success page
+        return redirect()->back()->with('status', 'School(s) successfully registered');
+    }
 
 }
